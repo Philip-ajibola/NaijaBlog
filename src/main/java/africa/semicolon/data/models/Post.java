0a22fb1e29b1dc92eecc362c182c1d81ba2d138e
philip <ajibolaphilip10@gmail.com>
2024-04-11 06:00:09 +0100

    "some update was made "

--- src/main/java/africa/semicolon/data/models/Post.java
+++ src/main/java/africa/semicolon/data/models/Post.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Post {
     @Id
     private String id;
-    private String poster;
+    private String author;
     private String title;
     private String content;
     private LocalDateTime dateCreated = LocalDateTime.now();
