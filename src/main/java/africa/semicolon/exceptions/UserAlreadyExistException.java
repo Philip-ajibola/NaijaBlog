bc9075efa7b71b7a50fbbcbf339dd864efc13b69
philip <ajibolaphilip10@gmail.com>
2024-04-11 06:04:25 +0100

    "some update was made "

--- src/main/java/africa/semicolon/exceptions/UserAlreadyExistException.java
+++ src/main/java/africa/semicolon/exceptions/UserAlreadyExistException.java
@@ -1,6 +1,6 @@
 package africa.semicolon.exceptions;
 
-public class UserAlreadyExistException extends FinalBlogExceptions{
+public class UserAlreadyExistException extends NaijaGossipsExceptions {
     public UserAlreadyExistException(String message) {
         super(message);
     }
