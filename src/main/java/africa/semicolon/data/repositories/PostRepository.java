d9970d71448e512e96433c74d57d33b8bd74df75
philip <ajibolaphilip10@gmail.com>
2024-04-11 06:02:10 +0100

    "some update was made "

--- src/main/java/africa/semicolon/data/repositories/PostRepository.java
+++ src/main/java/africa/semicolon/data/repositories/PostRepository.java
@@ -3,8 +3,6 @@ package africa.semicolon.data.repositories;
 import africa.semicolon.data.models.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 public interface PostRepository extends MongoRepository<Post, String> {
-    Post findByTitleAndPoster(String title,String poster);
+    Post findByTitleAndAuthor(String title, String poster);
 }
