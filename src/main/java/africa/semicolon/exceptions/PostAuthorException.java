642e8223aa52bca1c34fc01803fb6b2f8137228a
philip <ajibolaphilip10@gmail.com>
2024-04-11 06:00:39 +0100

    "some update was made "

---
+++ src/main/java/africa/semicolon/exceptions/PostAuthorException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.exceptions;
+
+public class PostAuthorException extends NaijaGossipsExceptions {
+    public PostAuthorException(String message) {
+        super(message);
+    }
+}
